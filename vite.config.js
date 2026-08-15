411bbc4ae456146b2748d8d97f0e5e361d6e1d32
Fooyin Sprout Admin <fooyin-sprout@example.com>
2026-08-15 09:52:46 +0800

    設定 GitHub Actions 自動部署至 GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   server: {
     port: 3000,
     host: true,
-    allowedHosts: true // Allow localtunnel & external hosts
+    allowedHosts: true
   },
   preview: {
     port: 3000,
